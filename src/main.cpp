5d4fa3c409233b9339bd4a78aea61cbbfc1b10c1
August Hedman <augustlh@icloud.com>
2024-03-06 12:21:01 +0100

    sdl setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
+#include <iostream>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_timer.h>
- 
-int main(int argc, char *argv[])
-{
-     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("error initializing SDL: %s\n", SDL_GetError());
+
+const int SCREEN_WIDTH = 640, SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[]) {
+    SDL_Init( SDL_INIT_EVERYTHING );
+    
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (window == NULL) {
+        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+        return 1;
     }
-    SDL_Window* win = SDL_CreateWindow("GAME",
-                                       SDL_WINDOWPOS_CENTERED,
-                                       SDL_WINDOWPOS_CENTERED,
-                                       1000, 1000, 0);
-    while (1)
-        ;
- 
+
+    SDL_Event windowEvent;
+
+    while(true) {
+        if (SDL_PollEvent(&windowEvent)) {
+            if (windowEvent.type == SDL_QUIT) {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
+
 }
+
