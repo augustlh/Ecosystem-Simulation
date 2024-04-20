cd3a3a5493bc790c2799cafe4fa552e8b2a3e1bd
Alexander <64310459+HjortFogh@users.noreply.github.com>
2024-04-20 13:58:56 +0200

    did stuff, fixed stuff, broke stuff :(

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,30 @@
 #include "Simulation.h"
 #include <iostream>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
+
+    if (argc < 2)
+    {
+        SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "Incorrect usage, use:\n%s config.yaml [fps=24]", argv[0]);
+        return -1;
+    }
+
+    std::string configPath = argv[1];
+    int renderFPS = 24;
+
+    if (argc == 3)
+        renderFPS = std::stoi(argv[2]);
+
     try
     {
-        Ecosim::Simulation simulation("C:/dev/projects/Ecosystem-Simulation/configs/simulations/firstSim.yaml");
-        simulation.Simulate(/*render fps, simulate fps*/);
+        Ecosim::Simulation simulation(configPath);
+        simulation.Simulate(renderFPS);
     }
     catch (const std::exception &e)
     {
-        std::cerr << e.what() << std::endl;
+        std::cerr << "Error:" << e.what() << std::endl;
         return -1;
     }
 
